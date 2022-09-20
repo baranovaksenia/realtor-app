15a999506bb18a0a5d7a5838edbb66cc9011d302
Ksenia <baranova-ksenia@mail.ru>
2022-09-20 18:51:37 +0300

    add reset password functionality

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -3,14 +3,28 @@ import SignInImg from '../assets/images/signIn.jpg';
 
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
-
+import { toast } from 'react-toastify';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 const ResetPassword = () => {
   const [email, setEmail] = useState('');
-
+  const navigate = useNavigate();
   const onChange = (e) => {
     setEmail(e.target.value);
   };
-
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      // email get from useState hook
+      await sendPasswordResetEmail(auth, email);
+      setEmail('');
+      toast.success('Email was sent');
+      navigate('/');
+    } catch (error) {
+      toast.error('Can not send reset the password');
+    }
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Reset Password</h1>
@@ -23,7 +37,7 @@ const ResetPassword = () => {
           />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <div className='relative mb-6'>
               <input
                 className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded 
